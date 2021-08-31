e213b11f2a7d11581290b0bc5fd141df37941678
ShazaAllam2001 <shazaallam90@gmail.com>
2021-08-31 19:45:58 +0200

    add Grid

---
+++ GUI/Grid.py
@@ -0,0 +1,86 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from dropArea import dropArea
+
+
+class Grid(QGraphicsScene):
+
+    def __init__(self, game: QMainWindow, parent=None):
+        super().__init__(parent)
+
+        """ Grid measures """
+        self.row = 8
+        self.col = 8
+        self.checkerSize = 48
+
+        self.margin = 10
+        self.xOffset = 8
+        self.yOffset = 5
+
+        """ Game playing """
+        self.game = game
+
+        self.dropAreas = []
+        self.checkers = []  
+        self.filledCheckers = [[0 for i in range(self.col)] for j in range(self.row)] 
+        
+        self.setBackgroundBrush(QColor("#346eeb"))
+
+
+    def drawBackground(self, painter: QPainter, rect: QRectF):
+       super().drawBackground(painter, rect)
+
+       top = int(rect.top())
+       bottom = int(rect.bottom())
+       left = int(rect.left())
+       right = int(rect.right())
+
+       Pen = QPen(QColor("#FFFFFF"))
+       Brush = QBrush(QColor("#FFFFFF"), Qt.BrushStyle.SolidPattern)
+       painter.setPen(Pen)
+       painter.setBrush(Brush)
+       
+       x = left
+       rectWidth = self.margin + self.checkerSize + self.xOffset/2
+       rectHeight = 30
+       for i in range(self.col):
+           rect = QRectF(x, top, rectWidth, rectHeight)
+           area = dropArea(rect, i)
+           self.dropAreas.append(area)
+           self.addItem(area)
+           painter.drawRect(rect)
+           x += rectWidth
+           if i+1 == 7:
+               rectWidth = self.margin + self.checkerSize + self.xOffset/2
+           else:
+               rectWidth = self.checkerSize + self.xOffset
+
+
+       firstTop = top + rectHeight + self.yOffset
+       firstLeft = left + self.margin
+       for x in range(firstLeft, right, self.checkerSize+self.xOffset):
+           row = []
+           for y in range(firstTop, bottom, self.checkerSize+self.yOffset):
+               checker = QRectF(x, y, self.checkerSize, self.checkerSize)
+               row.append(checker)
+               painter.drawEllipse(checker)
+           self.checkers.append(row)
+        
+       self.checkers = [value for value in self.checkers if value != []]
+    
+
+
+    
+
+
+
+
+    
+
+
+
+       
+
+
---
+++ GUI/dropArea.py
@@ -0,0 +1,85 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+
+HUMAN = False
+COMPUTER = True
+
+class dropArea(QGraphicsItem):
+
+    def __init__(self, area: QRectF, column: int, parent=None):
+        super().__init__(parent)
+
+        self.area = area
+        self.column = column
+        self.pressed = False
+        self.acceptPress = True
+        self.hovered = False
+        self.setAcceptHoverEvents(True)
+
+
+    def boundingRect(self) -> QRectF:
+        return self.area
+
+
+    def paint(self, painter: QPainter, option: 'QStyleOptionGraphicsItem', widget=None):
+        hoverPen = QPen(QColor("#000000"), 2)
+
+        redPen = QPen(QColor("#ff0004"))
+        redBrush = QBrush(QColor("#ff0004"), Qt.BrushStyle.SolidPattern)
+        yellowPen = QPen(QColor("#f5ec42"))
+        yellowBrush = QBrush(QColor("#f5ec42"), Qt.BrushStyle.SolidPattern)
+        
+        if self.hovered:
+            painter.setPen(hoverPen)
+            painter.drawRect(self.area)
+
+        if self.pressed or not self.acceptPress:
+           column = self.column
+           scene = self.scene()
+           checkers = scene.checkers[0:8]
+           checkersColumn = checkers[column]
+           filled = scene.filledCheckers
+
+           for row in range(7, -1, -1):
+                if filled[row][column] == 0:
+                  player = scene.game.turn
+                  if player == HUMAN:
+                    scene.filledCheckers[row][self.column] = 1
+                    scene.addEllipse(checkersColumn[row], redPen, redBrush)
+                  else:
+                    scene.filledCheckers[row][self.column] = 2
+                    scene.addEllipse(checkersColumn[row], yellowPen, yellowBrush)
+                  break
+        
+           scene.game.take_turns()
+                   
+           self.pressed = False
+
+
+    def hoverEnterEvent(self, event: 'QGraphicsSceneHoverEvent'):
+        self.hovered = True
+        self.update()
+
+    def hoverLeaveEvent(self, event: 'QGraphicsSceneHoverEvent'):
+        self.hovered = False
+        self.update()
+
+    def mousePressEvent(self, event: 'QGraphicsSceneMouseEvent'):
+        if self.acceptPress:
+          self.pressed = True  
+          self.update()
+
+    def mouseReleaseEvent(self, event: 'QGraphicsSceneMouseEvent'):
+        if self.acceptPress:
+          self.pressed = False  
+          self.update()
+        
+        
+       
+
+    
+    
+
+        
