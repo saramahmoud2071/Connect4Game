12c5358fe08607ca398c0ab8656fa08c348abe80
Mohamed Ibrahim <61321123+MohamedIbrahim99@users.noreply.github.com>
2021-09-01 22:46:58 +0200

    Update Minimax.py

--- GUI/solvingAlgorithms/Minimax.py
+++ GUI/solvingAlgorithms/Minimax.py
@@ -8,20 +8,22 @@ LENGTH, WIDTH = 8, 8
 # k is the limit that we will truncate the game tree after K levels evaluating the states using the heuristic function.
 # isPruning is a boolean indicates if we want to use alpha-beta pruning or not
 def make_decision(state, k, isPruning) :
-    new_state = maximize(state, k, isPruning, -10000, 10000)
-    return new_state
+    root = Node()
+    root.state = state
+    new_state = maximize(root, k, isPruning, -10000, 10000)
+    return new_state, root
 
 # Find the child state with the highest utility value
-def maximize(state, k, isPruning, alpha, beta) :
-    if (terminal_test(state, k)) :
-        return (None , eval(state))
+def maximize(node, k, isPruning, alpha, beta) :
+    if (terminal_test(node.state, k)) :
+        return (None , eval(node.state))
     
     max_child, max_utility = (None,-10000)
     
-    for child in make_children(state, 2) :
+    for child in make_children(node, 2) :
         null, utility = minimize(child, k-1, isPruning, alpha, beta)
         if utility > max_utility :
-            max_child, max_utility = (child, utility)
+            max_child, max_utility = (child.state, utility)
         if isPruning and max_utility >= beta :
             break
         if isPruning and max_utility > alpha :
@@ -30,16 +32,16 @@ def maximize(state, k, isPruning, alpha, beta) :
     return (max_child, max_utility)
 
 # Find the child state with the lowest utility value
-def minimize(state, k, isPruning, alpha, beta) :
-    if (terminal_test(state, k)) :
-        return (None , eval(state))
+def minimize(node, k, isPruning, alpha, beta) :
+    if (terminal_test(node.state, k)) :
+        return (None , eval(node.state))
     
     min_child, min_utility = (None,10000)
 
-    for child in make_children(state, 1) :
+    for child in make_children(node, 1) :
         null, utility = maximize(child, k-1, isPruning, alpha, beta)
         if utility < min_utility :
-            min_child, min_utility = (child, utility)
+            min_child, min_utility = (child.state, utility)
         if isPruning and min_utility <= alpha :
             break
         if isPruning and min_utility < beta :
@@ -54,16 +56,19 @@ def terminal_test(state, k) :
     return True
     
 # Find the possible moves for the next step
-def make_children(state, player) :
+def make_children(node, player) :
     children = []
     for i in range(LENGTH):
       for j in range(WIDTH):
-        if state[i][j] == 0:
-          if i < LENGTH-1 and state[i+1][j] != 0 or i == LENGTH-1:
-            new_state = deepcopy(state)
-            new_state[i][j] = player
-            children.append(new_state)
+        if node.state[i][j] == 0:
+            if i < LENGTH-1 and node.state[i+1][j] != 0 or i == LENGTH-1:
+                child = Node()
+                child.state = deepcopy(node.state)
+                child.state[i][j] = player
+                child.parent = node
+                children.append(child)
                
+    node.children = children
     return children
 
 def eval(state) :
