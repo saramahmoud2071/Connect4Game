467d682bd501e8f44a54c976839fa16d906122be
ShazaAllam2001 <61089681+ShazaAllam2001@users.noreply.github.com>
2021-09-03 11:45:01 +0200

    Add files via upload

--- GUI/solvingAlgorithms/Minimax.py
+++ GUI/solvingAlgorithms/Minimax.py
@@ -12,13 +12,13 @@ class Node:
     parent = None 
     children = []
     state = np.array([]) 
-    humanScore = 0
-    agentScore = 0
     depth = 0 
     utility = None 
     minimax = None 
     change = None 
     terminal = False 
+    humanScore = 0
+    agentScore = 0
     
 # Find the right move according to the Minimax search algorithm
 # k is the limit that we will truncate the game tree after K levels evaluating the states using the heuristic function.
@@ -40,7 +40,7 @@ def maximize(node: Node, k, isPruning, alpha, beta) :
     
     max_child, max_utility = (None,-10000)
     
-    for child in make_children(node, k-1, 2) :
+    for child in make_children(node, 2) :
         null, utility = minimize(child, k-1, isPruning, alpha, beta)
         if utility > max_utility :
             max_child, max_utility = (child.state, utility)
@@ -61,7 +61,7 @@ def minimize(node: Node, k, isPruning, alpha, beta) :
     
     min_child, min_utility = (None,10000)
 
-    for child in make_children(node, k-1, 1) :
+    for child in make_children(node, 1) :
         null, utility = maximize(child, k-1, isPruning, alpha, beta)
         if utility < min_utility :
             min_child, min_utility = (child.state, utility)
@@ -81,7 +81,7 @@ def terminal_test(state, k) :
     return True
     
 # Find the possible moves for the next step
-def make_children(node: Node, k: int, player) :
+def make_children(node: Node, player) :
     children = []
     for i in range(LENGTH):
       for j in range(WIDTH):
@@ -89,7 +89,12 @@ def make_children(node: Node, k: int, player) :
             if i < LENGTH-1 and node.state[i+1][j] != 0 or i == LENGTH-1:
                 new_state = deepcopy(node.state)
                 new_state[i][j] = player
-                child = Node(new_state, not node.minimax, j, k)
+                child = Node()
+                child.parent = node
+                child.state = new_state
+                child.minimax = not node.minimax
+                child.change = j
+                child.depth = node.depth + 1
                 children.append(child)
                
     node.children = children
