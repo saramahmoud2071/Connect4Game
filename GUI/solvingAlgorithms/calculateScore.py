6287e14f796e9167931c1ab7b63f48b0c983a6ba
saramahmoud2071 <89441343+saramahmoud2071@users.noreply.github.com>
2021-09-03 16:25:20 +0200

    Update calculateScore.py

--- GUI/solvingAlgorithms/calculateScore.py
+++ GUI/solvingAlgorithms/calculateScore.py
@@ -33,7 +33,8 @@ def rightDiagonal4(grid, player, i, j):
     if grid[i][j]== grid[i+1][j+1] == grid[i+2][j+2] == grid[i+3][j+3] == 1:
       return 1
 
-    elif grid[i][j]== grid[i+1][j+1] == grid[i+2][j+2] == grid[i+3][j+3] == 2:
+  else:
+    if grid[i][j]== grid[i+1][j+1] == grid[i+2][j+2] == grid[i+3][j+3] == 2:
       return 1
 
   return 0
